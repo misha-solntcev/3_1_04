fb39a65bd2b57c1828dc1d0be3591ae29a2fd71d
misha-solntcev <misha.solntcev@gmail.com>
2022-07-05 22:48:52 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Вариант 4
+В одномерном массиве, состоящем из n целых элементов, вычислить:
+ номер максимального элемента массива;
+ произведение элементов массива, расположенных между первым и вторым нулевыми элементами */
+
+namespace _3_1_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
