f98cda0e9513ab40efbaec9f6618502c556f43bd
misha-solntcev <misha.solntcev@gmail.com>
2022-07-05 23:13:08 +0800

    Номер максимального элемента массива.

--- Program.cs
+++ Program.cs
@@ -15,6 +15,22 @@ namespace _3_1_04
     {
         static void Main(string[] args)
         {
+            int[] elems = new int[] { 1, 21, 0, 2, 4, 5, 0, 6, 0, 8 };
+
+            // Номер максимального элемента массива:
+            int max = elems[0];
+            int num = 0;
+            for (int i = 0; i < elems.Length; i++)
+                if (elems[i] > max)
+                {
+                    max = elems[i];
+                    num = i;
+                }
+            Console.WriteLine($"max = {max}, num = {num}");
+            
+
+
+            Console.ReadKey();
         }
     }
 }
